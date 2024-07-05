8096c363cf513c048a6f12a876c9dbf4a174e73c
Willow Carlson-Huber <w@wolo.dev>
2024-07-05 00:30:54 -0500

    ASCII-ify characters

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #![warn(clippy::pedantic)]
 
-use serenity::{prelude::{GatewayIntents, EventHandler, Context}, Client, model::prelude::{Ready, Message}};
+use serenity::{
+	model::prelude::{Message, Ready},
+	prelude::{Context, EventHandler, GatewayIntents},
+	Client,
+};
 use uwuifier::uwuify_str_sse;
 
 const UWU_STRING: Option<&str> = option_env!("UWU_STRING");
@@ -16,11 +20,14 @@ async fn main() {
 	let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
 
 	// Client init
-	let mut client = Client::builder(&token, intents).event_handler(Handler).await.expect("create client");
+	let mut client = Client::builder(&token, intents)
+		.event_handler(Handler)
+		.await
+		.expect("create client");
 
 	// Run client
 	if let Err(why) = client.start().await {
-		eprintln!("Bot crash: {}", why);
+		eprintln!("Bot crash: {why}");
 	}
 }
 
@@ -34,7 +41,7 @@ impl EventHandler for Handler {
 
 	async fn message(&self, c: Context, m: Message) {
 		if let Err(e) = m.channel_id.start_typing(&c.http) {
-			eprintln!("Failed to start typing because {}", e);
+			eprintln!("Failed to start typing because {e}");
 		}
 		// Stop if the author is another bot.
 		if m.author.bot {
@@ -44,37 +51,51 @@ impl EventHandler for Handler {
 		// Stop if the channel is not uwu
 		if !m.channel(&c.http).await.map_or(false, |channel| {
 			channel.guild().map_or(false, |channel| {
-				channel.topic.map_or(false, |topic| topic.contains(UWU_STRING.unwrap_or(FALLBACK_UWU)))
+				channel.topic.map_or(false, |topic| {
+					topic.contains(UWU_STRING.unwrap_or(FALLBACK_UWU))
+				})
 			})
 		}) {
-			return
+			return;
 		}
-		
+
 		let content = m.content.clone();
-		let sender = m.author_nick(&c.http).await.unwrap_or_else(|| m.author.name.clone());
+		let content: String = content.chars().map(|c| (c as u8) as char).collect();
+		let sender = m
+			.author_nick(&c.http)
+			.await
+			.unwrap_or_else(|| m.author.name.clone());
 		let existing_embeds = m.embeds.clone();
 		let existing_attachments = m.attachments.clone();
 		if existing_embeds.is_empty() && existing_attachments.is_empty() {
 			if let Err(e) = m.delete(&c.http).await {
-				eprintln!("Failed to delete message because {}", e);
+				eprintln!("Failed to delete message because {e}");
 			};
 		} else {
-			return
+			return;
 		}
 		let uwu_sender = uwuify_str_sse(&sender);
 		let uwuized = uwuify_str_sse(&content);
 		let author_pfp = m.author.static_avatar_url();
-		if let Err(e) = m.channel_id.send_message(&c.http, |m| {
-			m.embed(|embed| {
-				embed.author(|author| {
-					let author = if let Some(url) = author_pfp {
-						author.icon_url(url)
-					} else {author};
-					author.name(uwu_sender)
-				}).description(uwuized)
+		if let Err(e) = m
+			.channel_id
+			.send_message(&c.http, |m| {
+				m.embed(|embed| {
+					embed
+						.author(|author| {
+							let author = if let Some(url) = author_pfp {
+								author.icon_url(url)
+							} else {
+								author
+							};
+							author.name(uwu_sender)
+						})
+						.description(uwuized)
+				})
 			})
-		}).await {
-			eprintln!("Failed to send message because {}", e);
+			.await
+		{
+			eprintln!("Failed to send message because {e}");
 		}
 	}
 }
